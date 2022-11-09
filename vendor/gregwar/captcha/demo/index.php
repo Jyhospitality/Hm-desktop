a3d93255b20673ef8a2fbdc038178f41949b15f4
Jyhospitality <98210165+Jyhospitality@users.noreply.github.com>
2022-11-09 12:55:45 +0530

    Add files via upload

---
+++ vendor/gregwar/captcha/demo/index.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TL8CPW3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+    <html>
+        <meta charset="utf-8" />
+    </html>
+    <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TL8CPW3"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
+        <h1>Captchas gallery</h1>
+        <?php for ($x=0; $x<8; $x++) { ?>
+            <?php for ($y=0; $y<5; $y++) { ?>
+                <img src="output.php?n=<?php echo 5*$x+$y; ?>" />
+            <?php } ?>
+            <br />
+        <?php } ?>
+    <!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ba9c352b156da713cb3c5f5/1fd980ev0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
+<!--Start of Tawk.to Script-->
+<script type="text/javascript">
+var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+(function(){
+var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+s1.async=true;
+s1.src='https://embed.tawk.to/5ba9c352b156da713cb3c5f5/1fd980ev0';
+s1.charset='UTF-8';
+s1.setAttribute('crossorigin','*');
+s0.parentNode.insertBefore(s1,s0);
+})();
+</script>
+<!--End of Tawk.to Script-->
+</body>
